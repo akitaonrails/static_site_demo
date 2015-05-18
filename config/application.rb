d94ffd1b54061311f5658b940356010e12a92455
AkitaOnRails <fabioakita@gmail.com>
2015-05-18 19:48:27 -0300

    Trying to make Rack Cache work in Heroku (cache missing and not storing for some reason)

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,5 @@ module StaticSiteDemo
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    config.cache_store = :dalli_store
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.cache_store = :dalli_store
   client = Dalli::Client.new((ENV["MEMCACHIER_SERVERS"] || "").split(","),
                              :username => ENV["MEMCACHIER_USERNAME"],
                              :password => ENV["MEMCACHIER_PASSWORD"],
